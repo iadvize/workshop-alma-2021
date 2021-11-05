27989addafb934b0593c8c993182e8d945061810
pierregaultier <pierre.gaultier@iadvize.com>
2021-11-05 15:36:12 +0100

    [PGA] refacto

--- IAdvizeRocketBackEnd/test/services/ShipServiceTest.scala
+++ IAdvizeRocketBackEnd/test/services/ShipServiceTest.scala
@@ -3,6 +3,7 @@ package services
 import models.IAdvizeShip
 import org.scalatestplus.play.PlaySpec
 import org.scalamock.scalatest.MockFactory
+import scala.util.{Failure, Success, Try}
 
 class ShipServiceTest extends PlaySpec with MockFactory {
 
@@ -27,6 +28,44 @@ class ShipServiceTest extends PlaySpec with MockFactory {
       result.equals(expectedOutputShip) mustBe true
     }
 
-  }
+    "be successful and return an spaceX rocket with spaceX request" in {
 
+      // Before
+      val spaceXRocketService: SpaceXRocketService = mock[SpaceXRocketService]
+      (spaceXRocketService.getRocket _)
+        .expects("id")
+        .returns(IAdvizeShip("1", "testRocket", "Appolo", 17000, "spaceX"))
+      val swapiStarshipService: SwapiStarshipService = mock[SwapiStarshipService]
+      val shipService: ShipService = new ShipService(spaceXRocketService, swapiStarshipService)
+
+      val expectedOutputShip = new IAdvizeShip("1", "testRocket", "Appolo", 17000, "spaceX")
+
+      // When
+      val result = shipService.getShip("spaceX", "id")
+
+      // Then
+      result.equals(expectedOutputShip) mustBe true
+    }
+
+    "return an error when requested ship provider does not exist" in {
+
+      // Before
+      val spaceXRocketService: SpaceXRocketService = mock[SpaceXRocketService]
+      val swapiStarshipService: SwapiStarshipService = mock[SwapiStarshipService]
+      val shipService: ShipService = new ShipService(spaceXRocketService, swapiStarshipService)
+
+
+      // When
+      val result = Try(
+        shipService.getShip("tesla", "id")
+      )
+
+      // Then
+      result match {
+        case Failure(e) => e mustBe a[UnsupportedOperationException]
+        case Success(v) => fail(s"Expected the test to fail but got $v instead")
+      }
+
+    }
+  }
 }
